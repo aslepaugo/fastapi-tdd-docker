3007b699f93a8bcab77aa473ec174b92b6902afc
Aliaksei Slepau <aslepaugo@gmail.com>
2021-03-09 22:41:45 +0100

    added postgresql and tortoise orm

--- project/app/config.py
+++ project/app/config.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 from functools import lru_cache
 
 
@@ -10,6 +10,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = os.getenv("ENVIRONMET", "dev")
     testing: bool = os.getenv("TESTING", 0)
+    database_url: AnyUrl = os.getenv("DATABASE_URL")
 
 @lru_cache()
 def get_settings() -> BaseSettings:
